a3d13f9b19366f8f769e0e3c63fe64f68d111825
sunithvs <sunithvs2002@gmail.com>
2022-12-29 00:50:46 +0530

    ADDED DOMAIN

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,16 @@
 # register domain
 from django.contrib import admin
 
-from home.models import SubDomain
+from home.models import SubDomain, Domain
 
 
 @admin.register(SubDomain)
-class DomainAdmin(admin.ModelAdmin):
+class SubDomainAdmin(admin.ModelAdmin):
     list_display = ['domain', 'proxy_pass']
     search_fields = ['domain', 'proxy_pass']
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
